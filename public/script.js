2c21e02c1e3e85135dc2ae956d6454e5e30fe703
Mehirion <Mehirion1@gmail.com>
2022-08-17 00:29:54 +0200

    html,js,css nearly done

--- public/script.js
+++ public/script.js
@@ -2,12 +2,24 @@ var search = document.querySelector('.city-search')
 let button = document.querySelector('.submit')
 let wind = document.querySelector('[data-wind]')
 let temperature = document.querySelector('[data-temperature]')
-let precip = document.querySelector('[data-precipitation]')
+let pressure = document.querySelector('[data-pressure]')
 const APIkey = 'b50b4b8c081de0f71583cf50189c755f'
+var skycons = new Skycons({"color": "pink"});
+skycons.play();
+
 
 button.addEventListener('click', function(){
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&appid=${APIkey}`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=${APIkey}`)
   .then(response => response.json())
-  .then(data => console.log(data)) 
-})
+  .then(data => {
+    var temperatureValue = data['main']['temp']
+    var windValue = data['wind']['speed']
+    var pressureValue = data['main']['pressure']
+  
 
+    temperature.innerHTML = temperatureValue;
+    wind.innerHTML = windValue;
+    pressure.innerHTML = pressureValue;
+  })
+
+  })

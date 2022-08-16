ef5d1a3303d900a24f8b176a4e5831a1f97f8278
Mehirion <Mehirion1@gmail.com>
2022-08-16 23:28:10 +0200

    ożyło

--- public/script.js
+++ public/script.js
@@ -1,16 +1,13 @@
-const button = document.querySelector('.submit')
-const search = document.querySelector('[data-city-search]')
-const wind = document.querySelector('[data-wind]')
-const temperature = document.querySelector('[data-temperature]')
-const precip = document.querySelector('[data-precipitation]')
+var search = document.querySelector('.city-search')
+let button = document.querySelector('.submit')
+let wind = document.querySelector('[data-wind]')
+let temperature = document.querySelector('[data-temperature]')
+let precip = document.querySelector('[data-precipitation]')
+const APIkey = 'b50b4b8c081de0f71583cf50189c755f'
 
-button.addEventListener('click', function(name){
-fetch('https://api.openweathermap.org/data/2.5/weather?q=city.name&appid=b50b4b8c081de0f71583cf50189c755f')
-.then(response => response.json())
-.then(data => console.log(data)) 
-
-
-const icon1 = new Skycons({ color: '#222' }) 
-  icon1.set('icon', 'clear-day')
-  icon1.play()
+button.addEventListener('click', function(){
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search.value}&appid=${APIkey}`)
+  .then(response => response.json())
+  .then(data => console.log(data)) 
 })
+
